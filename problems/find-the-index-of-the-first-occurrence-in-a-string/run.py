b8832e636290a8d2179bb301df25faa3d5f6abac
Praseetha-KR <praseetha04@gmail.com>
2024-12-23 00:12:28 +0530

    Problem: find-the-index-of-the-first-occurrence-in-a-string - add assertion

--- problems/find-the-index-of-the-first-occurrence-in-a-string/run.py
+++ problems/find-the-index-of-the-first-occurrence-in-a-string/run.py
@@ -2,15 +2,16 @@ from types import FunctionType, ModuleType
 
 
 def run(approach: ModuleType, display_io: FunctionType) -> None:
-    inputs = [
-        ["sadbutsad", "sad"],
-        ["leetcode", "leeto"],
-        ["hello", "ll"]
+    test_cases = [
+        [["sadbutsad", "sad"], [0]],
+        [["leetcode", "leeto"], [-1]],
+        [["hello", "ll"], [2]],
     ]
 
-    for params in inputs:
-        ret = approach.Solution().strStr(params[0], params[1])
+    for input, expected_output in test_cases:
+        ret = approach.Solution().strStr(input[0], input[1])
         display_io(
-            input=f"haystack = {params[0]}, needle = {params[1]}",
+            input=f"haystack = {input[0]}, needle = {input[1]}",
             output=ret,
         )
+        assert ret == expected_output[0]
