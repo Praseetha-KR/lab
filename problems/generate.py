d7dcc8b5951c62f43c1a85265c194e04279d1b88
Praseetha-KR <praseetha04@gmail.com>
2024-12-25 00:11:25 +0530

    Update generate template to include assert

--- problems/generate.py
+++ problems/generate.py
@@ -9,14 +9,18 @@ RUN_PY_TPL = """from types import FunctionType, ModuleType
 
 
 def run(approach: ModuleType, display_io: FunctionType) -> None:
-    inputs = []
+    test_cases = [
+        [[], []],
+    ]
 
-    for params in inputs:
-        ret = approach.Solution().fn(params[0])
+    for input, expected_output in test_cases:
+        ret = approach.Solution().fn(input[0])
         display_io(
-            input=f"i = {params[0]}",
+            input=f"i = {input[0]}",
             output=ret,
         )
+
+        assert ret == expected_output[0]
 """
 
 def get_dir_tree(dir: str) -> None:
