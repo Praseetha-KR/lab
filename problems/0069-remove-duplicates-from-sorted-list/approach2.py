185244c254b6ba1411ca533c29e97a5ada34703e
Praseetha-KR <praseetha04@gmail.com>
2025-01-27 20:02:09 +0530

    Problem: 0069-remove-duplicates-from-sorted-list approach 2

---
+++ problems/0069-remove-duplicates-from-sorted-list/approach2.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from .base import ListNode
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        while current:
+            while current.next and current.val == current.next.val:
+                current.next = current.next.next
+            current = current.next
+        return head
